026497d91304a049adcf95b5c23b55d496bc4107
Mitch Beaumont <mitch.beaumont@gmail.com>
2016-03-25 21:53:24 +1100

    Add capistrano

---
+++ config/deploy/production.rb
@@ -0,0 +1,6 @@
+set :stage, :production
+
+# Replace 127.0.0.1 with your server's IP address!
+server 'ec2-52-63-26-33.ap-southeast-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app}
+
+ec2-52-63-26-33.ap-southeast-2.compute.amazonaws.com
