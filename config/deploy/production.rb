d2dcf4ad555f12e10cfe74c46d138c254b979c37
Mitch Beaumont <mitch.beaumont@gmail.com>
2016-03-25 22:01:03 +1100

    Update ruby verison in Capfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server 'ec2-52-63-26-33.ap-southeast-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app}
-
-ec2-52-63-26-33.ap-southeast-2.compute.amazonaws.com
+server "ec2-52-63-26-33.ap-southeast-2.compute.amazonaws.com", user: 'deploy', roles: %w{web app}
